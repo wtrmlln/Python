6a6da144b9301cbe816a3309e76fdca006810360
wtrmlln <ama20012001@gmail.com>
2022-05-13 16:26:09 +0400

    file_loading_funcs refactoring

--- FileUploadingBot/file_loading_funcs.py
+++ FileUploadingBot/file_loading_funcs.py
@@ -4,85 +4,42 @@ from file_processing_funcs import add_cookies, authorization, load_file
 def price_loading(filename):
     driver = initialize_chrome()
     cookies = load_cookies()
-    #Поиск необходимого сайта    
     if 'Торуда' in filename:
-        site_name = 'Торуда'
-        #Авторизация
-        driver.get('https://toruda.ru/netcat/add.php?inside_admin=&catalogue=1&sub=3&cc=10004')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
+        list_name_link = ['Торуда', 'https://toruda.ru/netcat/add.php?inside_admin=&catalogue=1&sub=3&cc=10004']
     elif 'ПМК' in filename:
-        site_name = 'ПМК'
-        #Авторизация
-        driver.get('https://prommashkomplekt.ru/netcat/add.php?catalogue=1&sub=3&cc=10004')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
+        list_name_link = ['ПМК', 'https://prommashkomplekt.ru/netcat/add.php?catalogue=1&sub=3&cc=10004']
     elif 'DDI' in filename:
-        site_name = 'DDI'
-        #Авторизация
-        driver.get('https://d-d-i.ru/netcat/add.php?catalogue=1&sub=3&cc=10004')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
-    elif ' Н ' in filename:
-        site_name = 'Компкресла'
-        #Авторизация
-        driver.get('https://kompkresla.ru/netcat/?catalogue=2&sub=106&cc=9346')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
+        list_name_link = ['DDI', 'https://d-d-i.ru/netcat/add.php?catalogue=1&sub=3&cc=10004']
+    elif 'DDI' in filename:
+        list_name_link = [' Н ', 'https://kompkresla.ru/netcat/?catalogue=2&sub=106&cc=9346']
     elif 'Кабинетоф' in filename:
-        site_name = 'Кабинетоф'
-        #Авторизация
-        driver.get('http://kabinetof.ru/netcat/?catalogue=1&sub=81&cc=4489&curPos=0')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
+        list_name_link = ['Кабинетоф', 'http://kabinetof.ru/netcat/?catalogue=1&sub=81&cc=4489&curPos=0']
     else:
-        site_name = 'SOT'
-        #Авторизация
-        driver.get('http://s-o-t.ru/netcat/add.php?catalogue=1&sub=3&cc=10004')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
+        list_name_link = ['SOT', 'http://s-o-t.ru/netcat/add.php?catalogue=1&sub=3&cc=10004']
+
+    site_name = list_name_link[0]
+    driver.get(list_name_link[1])
+    if cookies != None:
+        add_cookies(cookies, driver, site_name)
+    authorization(site_name, driver)
+    load_file(filename, site_name, driver)
 
 def stocks_loading(site_name, filename):
     #Инициализация Chrome
+    if 'Торуда' in site_name:
+        name_link = 'https://toruda.ru/netcat/add.php?inside_admin=&catalogue=1&sub=3&cc=1195760'
+    elif 'ПМК' in site_name:
+        name_link = 'https://prommashkomplekt.ru/netcat/add.php?catalogue=1&sub=3&cc=431514'
+    elif 'Компкресла' in site_name:
+        name_link = 'https://kompkresla.ru/netcat/?catalogue=2&sub=106&cc=11845'
+    else:
+        name_link = 'http://s-o-t.ru/netcat/add.php?catalogue=1&sub=3&cc=1195760'
+    
     driver = initialize_chrome()
     cookies = load_cookies()
-    if 'Торуда' in site_name:
-        driver.get('https://toruda.ru/netcat/add.php?inside_admin=&catalogue=1&sub=3&cc=1195760')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-            driver.refresh()
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
-      
-    elif 'ПМК' in site_name:
-        driver.get('https://prommashkomplekt.ru/netcat/add.php?catalogue=1&sub=3&cc=431514')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
-
-    elif 'Компкресла' in site_name:
-        driver.get('https://kompkresla.ru/netcat/?catalogue=2&sub=106&cc=11845')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
-
-    else:
-        driver.get('http://s-o-t.ru/netcat/add.php?catalogue=1&sub=3&cc=1195760')
-        if cookies != None:
-            add_cookies(cookies, driver, site_name)
-        authorization(site_name, driver)
-        load_file(filename, site_name, driver)
+    driver.get(name_link)
+    if cookies != None:
+        add_cookies(cookies, driver, site_name)
+        driver.refresh()
+    authorization(site_name, driver)
+    load_file(filename, site_name, driver)
